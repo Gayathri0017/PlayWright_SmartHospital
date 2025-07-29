e101aec9af162ee3f37c178081afca05b0577f8d
Gayathri0017 <gayathri4866@gmail.com>
2025-07-29 15:43:44 +0530

    Added Trace Viwer

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -18,7 +18,7 @@ BeforeAll(async function () {
 Before(async function ({ pickle }) {
   const scenarioName = pickle.name;
   context=await browser.newContext();
-  
+  await context.tracing.start({ screenshots: true, snapshots: true });
   const page=await context.newPage();
   const apiRequestContext=await playwrightRequest.newContext();
   pageFixture.page=page;
@@ -51,23 +51,45 @@ Before(async function ({ pickle }) {
     this.currentTestData = matchedData;
   } else {
     this.currentTestData = {};
-    // console.warn(`⚠️ No test data found for scenario: "${scenarioName}" — continuing without it.`);
   }
   pageFixture.logger=createLogger(options(scenarioName));
 });
 
-After(async function ({ pickle, result }) {
-  console.log(`Scenario "${pickle.name}" finished with status: ${result?.status}`);
-  if (result?.status === Status.FAILED) {
-    const img = await pageFixture.page.screenshot({
-      path: `./test-result/screenshots/${pickle.name}.png`,
-      type: "png",
+// After(async function ({ pickle, result }) {
+//   console.log(`Scenario "${pickle.name}" finished with status: ${result?.status}`);
+//   if (result?.status === Status.FAILED) {
+//     const img = await pageFixture.page.screenshot({
+//       path: `./test-result/screenshots/${pickle.name}.png`,
+//       type: "png",
+//     });
+//     await this.attach(img, "image/png");
+//   }
+//   await pageFixture.page.close();
+//   await context.close();
+// });
+After(async function({ pickle, result }) {
+  const scenarioName=pickle.name.replace(/\s+/g, "_");
+  console.log(`Scenario "${pickle.name}" status: ${result?.status}`);
+
+  if (result?.status===Status.FAILED) {
+    // Save screenshot
+    const screenshot=await pageFixture.page.screenshot({
+      path:`./test-result/screenshots/${scenarioName}.png`,
+      type:"png",
     });
-    await this.attach(img, "image/png");
+    await this.attach(screenshot, "image/png");
+    // Save trace
+    const tracePath=`./test-result/traces/${scenarioName}.zip`;
+    await context.tracing.stop({ path: tracePath });
+    await this.attach(`Trace: ${tracePath}`);
+  }
+  else{
+    await context.tracing.stop();
   }
   await pageFixture.page.close();
   await context.close();
 });
+
 
 AfterAll(async function () {
   await browser.close();
